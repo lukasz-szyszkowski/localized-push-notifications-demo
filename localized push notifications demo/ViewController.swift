ea41797ded0e35c8538788b2175c605c26f18168
Łukasz Szyszkowski <lukasz.szyszkowski@holdapp.pl>
2021-02-13 18:40:36 +0100

    Project Initialization

---
+++ localized push notifications demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  localized push notifications demo
+//
+//  Created by Łukasz Szyszkowski on 09/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
